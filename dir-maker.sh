e9ba601bdf822199ba2c261406cbe11b225ebf29
Nayan Makwana <nayan.dj95@gmail.com>
2022-06-22 21:41:52 +0530

    Updated

--- dir-maker.sh
+++ dir-maker.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-password="8049renu"
-pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-sudo su
-echo "$pass"
-mkdir /etc/teleport
-cd /etc/teleport
-touch test.txt
+mkdir -p nayan
